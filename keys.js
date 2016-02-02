f94376feb120d537d6eca6aaf9e3852fb5082162
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-01 19:32:00 -0500

    trying to add random tweets

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'rAS4BCE79O4876577PRsFoxL6',
+  consumer_secret: 'bzGa0jHDbpkggN7BumtFHim0JVQD4KN1QmgwyMciYDFbajmhjc',
+  access_token_key: '384633551-JVAx22Yt4HKlctmNQTMlXF2q6csYmLwcmwAxexuj',
+  access_token_secret: 'ROTvuVSGj2aM5qeu0Re7PhgvVmJuYlgsOQL4LZWWRLzd8',
+}
--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 var http = require('http');
+var Twitter = require('twitter');
+var keys = require("./keys.js");
+
+// function getTweets () {
+//   var client = new Twitter({
+//     consumer_key: keys.twitterKeys.consumer_key,
+//     consumer_secret: keys.twitterKeys.consumer_secret,
+//     access_token_key: keys.twitterKeys.access_token_key,
+//     access_token_secret: keys.twitterKeys.access_token_secret
+//   });
+
+//   client.get('statuses/user_timeline', {screen_name: 'paulducsantos'},  function(error, tweet, response){
+//     if(error) throw error;
+//     for (var i = 0; i < 20; i++) {
+//       console.log(tweet[i].text);
+//       writeToFile("\n" + tweet[i].text + "\n");
+//       console.log("Tweeted on: " + tweet[i].created_at + "\n");
+//       writeToFile("Tweeted on: " + tweet[i].created_at + "\n")
+//     }
+//     // console.log(tweet[0].id);  // Tweet body. 
+//     //console.log(response);  // Raw response object. 
+//   });
+// }
+
+
 
 const PORT = 7000;
 
@@ -6,7 +31,7 @@ var goodPhrase = ['You are so cool', 'You aren\'t a loser', 'Don\'t let your dre
 
 var handleRequest = function (req, res) {
   var randomNum = Math.floor(Math.random()*goodPhrase.length);
-  res.end(goodPhrase[randomNum]);
+  res.end(goodPhrase[randomNum], getTweets());
 }
 
 var server = http.createServer(handleRequest);
